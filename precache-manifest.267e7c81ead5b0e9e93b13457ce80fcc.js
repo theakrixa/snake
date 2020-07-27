9862a6209921d0b3b201ecda4076f137adc6158e
Aditya Pratap <pratapaditya84@gmail.com>
2020-07-27 11:16:50 +0530

    Updates

---
+++ precache-manifest.267e7c81ead5b0e9e93b13457ce80fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f269e15fa40582262af372bd8e0199",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "16adb2c5cce91a6e05f7",
+    "url": "/snake/static/css/main.091ae1e2.chunk.css"
+  },
+  {
+    "revision": "12eb9a0048fedbe33580",
+    "url": "/snake/static/js/2.23ebe0d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake/static/js/2.23ebe0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16adb2c5cce91a6e05f7",
+    "url": "/snake/static/js/main.e9e8180c.chunk.js"
+  },
+  {
+    "revision": "330302f1424891b30bb2",
+    "url": "/snake/static/js/runtime-main.f5fb9ecf.js"
+  }
+]);
